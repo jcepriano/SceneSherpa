471bb2f2beaf568e2daa573f1d71e53d6fe648fb
jkimball54 <jeremyck54@gmail.com>
2023-09-19 14:26:58 -0600

    users added

--- SceneSherpa/DataAccess/SceneSherpaContext.cs
+++ SceneSherpa/DataAccess/SceneSherpaContext.cs
@@ -1,10 +1,12 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using SceneSherpa.Models;
 
 namespace SceneSherpa.DataAccess
 {
     public class SceneSherpaContext : DbContext
     {
-        //Dbsets
+        DbSet<User> Users { get; set; }
+
         public SceneSherpaContext(DbContextOptions<SceneSherpaContext> options) : base(options)
         {
 
