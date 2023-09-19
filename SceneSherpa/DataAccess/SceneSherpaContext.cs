423f3f54a6673bd6852276cd6ebcc06ebfa83c54
jkimball54 <jeremyck54@gmail.com>
2023-09-19 14:22:20 -0600

    setup database connection

---
+++ SceneSherpa/DataAccess/SceneSherpaContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SceneSherpa.DataAccess
+{
+    public class SceneSherpaContext : DbContext
+    {
+        //Dbsets
+        public SceneSherpaContext(DbContextOptions<SceneSherpaContext> options) : base(options)
+        {
+
+        }
+    }
+}
---
+++ SceneSherpa/Migrations/20230919201830_initSetup.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SceneSherpa.Migrations
+{
+    /// <inheritdoc />
+    public partial class initSetup : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
