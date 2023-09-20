54fc5d1dbd7fc14f8bb1944d4b16b8492ff35d59
bradenasmith2 <bradenasmith@icloud.com>
2023-09-20 12:20:50 -0500

    Completed actions for returning a Login Form, as well as the action to complete verify the user's credentials to login.

--- SceneSherpa/Controllers/UsersController.cs
+++ SceneSherpa/Controllers/UsersController.cs
@@ -2,6 +2,8 @@
 using Microsoft.EntityFrameworkCore;
 using SceneSherpa.DataAccess;
 using SceneSherpa.Models;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace SceneSherpa.Controllers
 {
@@ -39,6 +41,40 @@ namespace SceneSherpa.Controllers
             return Redirect($"/users/{user.Id}");
         }
 
+        [Route("/users/login")]
+        public IActionResult LoginForm()
+        {
+            ViewData["FailedLogin"] = TempData["FailedLogin"];
+            return View();
+        }
+
+        [HttpPost]
+        [Route("/users/login/attempt")]
+        public IActionResult LoginAttempt(string username, string password)
+        {
+            HashAlgorithm sha = SHA256.Create();
+            byte[] firstInputBytes = Encoding.ASCII.GetBytes(password);
+            byte[] firstInputDigested = sha.ComputeHash(firstInputBytes);
+
+            StringBuilder firstInputBuilder = new StringBuilder();
+            foreach (byte b in firstInputDigested)
+            {
+                Console.Write(b + ", ");
+                firstInputBuilder.Append(b.ToString("x2"));
+            }
+            string value = firstInputBuilder.ToString();
+
+            if (_context.Users.Where(e => e.Username == username && e.Password == value).Any())
+            {
+                User user = _context.Users.Where(e => e.Username == e.Username).Single();
+                return Redirect($"/users/{user.Id}");
+            }
+            else
+            {
+                TempData["FailedLogin"] = "Either your password or username is incorrect, please try again.";
+                return Redirect("/users/login");
+            }
+        }
         
         [Route("/Users/{id:int}")]
         public IActionResult Show(int id)
@@ -83,7 +119,5 @@ namespace SceneSherpa.Controllers
             //Change the redirect to home page? 
             return RedirectToAction("index");
         }
-
-
     }
 }
