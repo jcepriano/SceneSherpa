0b95475c251a0947934d58f15ceff2b71549fb4a
jcepriano <jamescepriano@gmail.com>
2023-09-20 13:09:56 -0700

    created actions

--- SceneSherpa/Controllers/ReviewsController.cs
+++ SceneSherpa/Controllers/ReviewsController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using SceneSherpa.DataAccess;
+using SceneSherpa.Models;
 
 namespace SceneSherpa.Controllers
 {
@@ -14,6 +15,24 @@ namespace SceneSherpa.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+
+        [Route("media/{id:int}/review")]
+        public IActionResult Edit(int reviewId)
+        {
+            var user = _context.Users.Find(reviewId);
+            return View(user);
+        }
+
+        [HttpPost]
+        [Route("media/{id:int}")]
+        public IActionResult Update(int id, Review review)
+        {
+            review.Id = id;
+            _context.Reviews.Update(review);
+            _context.SaveChanges();
+
+            return Redirect("/media/{id:int}");
         }
     }
 }
