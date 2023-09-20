9ee3dd2403a2172e530d5506e6e55c37ff91c9c4
jkimball54 <jeremyck54@gmail.com>
2023-09-20 14:03:11 -0600

    finished create route for media

--- SceneSherpa/Controllers/ReviewsController.cs
+++ SceneSherpa/Controllers/ReviewsController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.TestPlatform.CommunicationUtilities;
 using SceneSherpa.DataAccess;
+using SceneSherpa.Models;
 
 namespace SceneSherpa.Controllers
 {
@@ -22,6 +24,20 @@ namespace SceneSherpa.Controllers
         {
             var media = _context.Media.Where(m => m.Id == mediaId).Include(m => m.Reviews).First();
             return View(media);
+        }
+
+        [HttpPost]
+        [Route("/Media/{mediaId:int}/Reviews")]
+        public IActionResult Create(int mediaId, Review review)
+        {
+            var media = _context.Media
+                .Where(m => m.Id == mediaId)
+                .Include(m => m.Reviews)
+                .First();
+            review.CreatedAt = DateTime.Now.ToUniversalTime();
+            media.Reviews.Add(review);
+            _context.SaveChanges();
+            return Redirect($"/Media/{mediaId}");
         }
 
     }
