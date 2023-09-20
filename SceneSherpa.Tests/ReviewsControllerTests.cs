04bb1ba3a2527468927fb7fed95044297e21b4dd
bradenasmith2 <bradenasmith@icloud.com>
2023-09-20 10:32:49 -0500

    Injected _factory to each ControllerTest

--- SceneSherpa.Tests/ReviewsControllerTests.cs
+++ SceneSherpa.Tests/ReviewsControllerTests.cs
@@ -1,7 +1,32 @@
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using SceneSherpa.Controllers;
+using SceneSherpa.DataAccess;
+
 namespace SceneSherpa.Tests
 {
-    public class UnitTest1
+    [Collection("Async")]
+    public class ReviewsControllerTests : IClassFixture<WebApplicationFactory<Program>>
     {
+        private readonly WebApplicationFactory<Program> _factory;
+
+        public ReviewsControllerTests(WebApplicationFactory<Program> factory)
+        {
+            _factory = factory;
+        }
+
+        private SceneSherpaContext GetDbContext()
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<SceneSherpaContext>();
+            optionsBuilder.UseInMemoryDatabase("TestDatabase");
+
+            var context = new SceneSherpaContext(optionsBuilder.Options);
+            context.Database.EnsureDeleted();
+            context.Database.EnsureCreated();
+
+            return context;
+        }
+
         [Fact]
         public void Test1()
         {
