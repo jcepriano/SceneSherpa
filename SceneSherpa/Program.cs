bcb2a75c308c6d980b52fac7bd25091818c892d0
jkimball54 <jeremyck54@gmail.com>
2023-09-26 13:58:51 -0600

    404 Is now custom

--- SceneSherpa/Program.cs
+++ SceneSherpa/Program.cs
@@ -25,10 +25,26 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
+    //Override Exception Page
+    app.UseDeveloperExceptionPage();
+}
+else
+{
     app.UseExceptionHandler("/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+//Handle 404 Routing
+app.Use(async (context, next) =>
+{
+    await next();
+    if (context.Response.StatusCode == 404)
+    {
+        context.Request.Path = "/Media/48";
+        await next();
+    }
+});
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
