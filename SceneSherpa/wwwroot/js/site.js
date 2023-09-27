58b2d6961c7c0799273c328a132f176e459cceab
jcepriano <jamescepriano@gmail.com>
2023-09-27 13:11:39 -0700

    js added/not working

--- SceneSherpa/wwwroot/js/site.js
+++ SceneSherpa/wwwroot/js/site.js
@@ -25,3 +25,21 @@
         $("#filter-count").text("Number of Filter = " + count);
     });
 });
+
+// JavaScript function to store and restore scroll position
+function storeAndRestoreScrollPosition() {
+    // Store the current scroll position in localStorage
+    localStorage.setItem('scrollPosition', window.scrollY);
+
+    // Attach an event listener to the window's load event
+    window.addEventListener('load', function () {
+        // Retrieve the stored scroll position from localStorage
+        var scrollPosition = localStorage.getItem('scrollPosition');
+
+        // If a scroll position was stored, scroll to it
+        if (scrollPosition !== null) {
+            window.scrollTo(0, parseInt(scrollPosition));
+            localStorage.removeItem('scrollPosition'); // Clear the stored position
+        }
+    });
+}
