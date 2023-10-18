a753b0d616d473576df8b8a029774a35fe7385f4
jcepriano <jamescepriano@gmail.com>
2023-10-18 12:50:06 -0700

    /

--- SceneSherpa/Models/User.cs
+++ SceneSherpa/Models/User.cs
@@ -8,21 +8,13 @@ namespace SceneSherpa.Models
     {
         public int Id { get; set; }
 
-        [Required(ErrorMessage = "Name is Required")]
         [StringLength(747, ErrorMessage = "Name cannot exceed 747 characters")]
         public string Name { get; set; }
 
-        [Required(ErrorMessage = "Username is Required")]
         [StringLength(100, ErrorMessage = "Name cannot exceed 100 characters")]
         public string Username { get; set; }
-
-        [Required(ErrorMessage = "Email is Required")]
         public string Email { get; set; }
-
-        [Required(ErrorMessage = "Password is Required")]
         public string Password { get; set; }
-
-        [Required(ErrorMessage = "Age is Required")]
         public int Age { get; set; }
 
         public List<Media>? CurrentWatch { get; set; } = new List<Media>();
